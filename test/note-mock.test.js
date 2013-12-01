22fd72fe93042cb2edf117de119713f702c90884
Jean Lauliac <lauliacj@gmail.com>
2013-12-01 11:50:44 +0100

    Add NodeMock test

---
+++ test/note-mock.test.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var chai = require('chai');
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var nodeMock = require('../lib/node-mock.js');
+
+var should = chai.should();
+chai.use(sinonChai);
+
+describe('NodeMock', function() {
+    var network, node1, node2, node3;
+
+    before(function() {
+        network = new nodeMock.NetworkMock();
+        node1 = new nodeMock.NodeMock(network);
+        node2 = new nodeMock.NodeMock(network);
+        node3 = new nodeMock.NodeMock(network);
+    });
+
+    after(function() {
+        node3.close();
+        node2.close();
+        node1.close();
+    });
+
+    describe('#ping()', function() {
+        it('should ping', function(cb) {
+            var pingSpy = sinon.spy(function(cb) {
+                cb();
+            });
+            node2.once('ping', pingSpy);
+            node1.ping(node2.endpoint, function(err) {
+                should.not.exist(err);
+                pingSpy.should.be.have.been.called;
+                cb();
+            });
+        });
+
+        it('should timeout when no answer', function(cb) {
+            var pingSpy = sinon.spy(function(cb) {
+            });
+            node2.once('ping', pingSpy);
+            node1.ping(node2.endpoint, function(err) {
+                should.exist(err);
+                err.code.should.equal('ETIMEDOUT');
+                pingSpy.should.be.have.been.called;
+                cb();
+            });
+        });
+
+
+        it('should timeout when no remote node', function(cb) {
+            node1.ping('invalid endpoint', function(err) {
+                should.exist(err);
+                err.code.should.equal('ETIMEDOUT');
+                cb();
+            });
+        });
+    });
+});
