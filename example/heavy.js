c432741ed41f4e56057ea077bcd3998e0c332e58
Jean Lauliac <lauliacj@gmail.com>
2013-12-22 17:08:44 +0100

    Add #close() on Dht

--- example/heavy.js
+++ example/heavy.js
@@ -31,43 +31,74 @@ function spawnSome(dhts, seeds, nb, cb) {
     });
 }
 
-var WORDS = ['foo', 'bar', 'baz', 'glo', 'ick'];
+var WORDS = ['foo', 'bar', 'baz']; //, 'glo', 'ick'];
 var locals = {};
+
+
+function setSome(dhts, key, ix, cb) {
+    var value = Math.random() * 256 | 0;
+    console.log('[master] set %s=%d on #%s', key, value,
+                dhts[ix].rpc.endpoint);
+    locals[key] = value;
+    dhts[ix].set(key, value, function (err) {
+        if (err) return console.log('  ERROR! %s', err.message);
+        cb();
+    });
+}
+
+function getSome(dhts, key, ix, cb) {
+    console.log('[master] get %s on #%s', key, dhts[ix].rpc.endpoint);
+    dhts[ix].get(key, function (err, value) {
+        if (err) return console.log('  ERROR! %s', err.message);
+        if (value !== locals[key]) {
+            console.log('/!\\ warning:' +
+                        ' expected %s=%d but got %s', key,
+                        locals[key], value);
+        }
+        cb();
+    });
+}
+
+function newNode(dhts, cb) {
+    var seeds = [dhts[Math.random() * dhts.length | 0].rpc.endpoint];
+    spawnNode('local:' + nextPort, seeds, function (err, dht) {
+        ++nextPort;
+        if (err) return console.log('  ERROR! %s', err.message);
+        dhts.push(dht);
+        console.log('[master] NEW node! %s', dht.rpc.endpoint);
+        cb();
+    });
+}
+
+function oldNode(dhts, ix, cb) {
+    console.log('[master] REMOVING node! %s', dhts[ix].rpc.endpoint);
+    dhts[ix].close();
+    dhts.splice(ix, 1);
+    process.nextTick(function () {
+        cb();
+    });
+}
+
+var degree = 4;
 
 function doStuff(dhts, count) {
     setTimeout(function () {
         if (count === 0) return;
         var key = WORDS[Math.random() * WORDS.length | 0];
         var ix = Math.random() * dhts.length | 0;
-        if (Math.random() < 0.5) {
-            var value = Math.random() * 256 | 0;
-            console.log('[master] set %s=%d on #%s', key, value,
-                        dhts[ix].rpc.endpoint);
-            locals[key] = value;
-            dhts[ix].set(key, value, function (err) {
-                if (err) return console.log('  ERROR! %s', err.message);
-                doStuff(dhts, count - 1);
-            });
-        } else if (Math.random() < 1) {
-            console.log('[master] get %s on #%s', key, dhts[ix].rpc.endpoint);
-            dhts[ix].get(key, function (err, value) {
-                if (err) return console.log('  ERROR! %s', err.message);
-                if (value !== locals[key]) {
-                    console.log('/!\\ warning:' +
-                                ' expected %s=%d but got %s', key,
-                                locals[key], value);
-                }
-                doStuff(dhts, count - 1);
-            });
-        } else {
-            var seeds = [dhts[Math.random() * dhts.length | 0].rpc.endpoint];
-            spawnNode('local:' + nextPort, seeds, function (err, dht) {
-                ++nextPort;
-                if (err) return console.log('  ERROR! %s', err.message);
-                dhts.push(dht);
-                console.log('[master] NEW node! %s', dht.rpc.endpoint);
-                doStuff(dhts, count - 1);
-            });
+        var action = Math.random() * degree | 0;
+        function cont() {
+            doStuff(dhts, count - 1);
+        }
+        switch (action) {
+            case 0:
+                return setSome(dhts, key, ix, cont);
+            case 1:
+                return getSome(dhts, key, ix, cont);
+            case 2:
+                return newNode(dhts, cont);
+            case 3:
+                return oldNode(dhts, ix, cont);
         }
     }, (Math.random() * 20) | 0);
 }
